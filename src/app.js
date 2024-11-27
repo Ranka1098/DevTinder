baee0fb483abb4299008b98d5a8eaa53fddbefbb
Ashokkumar Vaishnav <ashokranka30@gmail.com>
2024-11-27 15:59:54 +0530

    middleware admin Authrization

--- src/app.js
+++ src/app.js
@@ -12,83 +12,19 @@ app.listen(500, () => {
   console.log("server listen incoming call on port number 3000");
 });
 
-// different request handle
-app.use("/test", (req, res) => {
-  res.send("test call");
+// middleware authrizaton example
+app.use("/admin", (req, res, next) => {
+  const token = "xyz";
+  const isAuthorized = token === "xyz";
+  if (!isAuthorized) {
+    res.status(401).send("unauthorized request");
+  } else {
+    next();
+  }
 });
-
-app.use("/hello", (req, res) => {
-  res.send("hello call");
+app.get("/admin/getData", (req, res) => {
+  res.send("user data is send");
 });
-
-// all http call form server
-
-app.get("/user", (req, res) => {
-  res.send("get call from 3000");
-});
-
-app.post("/user", (req, res) => {
-  res.send("post call from 3000");
-});
-
-app.patch("/user", (req, res) => {
-  res.send(" patch call frm 3000");
-});
-
-app.put("/user", (req, res) => {
-  res.send(" put call frm 3000");
-});
-
-app.delete("/user", (req, res) => {
-  res.send(" delete call frm 3000");
-});
-
-// advance routing
-
-// 1.optional routing using question mark (?)
-// get call route work /abc ya /ac
-app.get("/ab?c", (req, res) => {
-  res.send("advance rounting call ab?c");
-});
-
-//2.match pattern using plus mark (+)
-//get call match pattern ending with bc
-//call /abc ya /abbbc ya /abbbbc
-app.get("/ab+c", (req, res) => {
-  res.send("advance rounting call ab+c");
-});
-
-//3.between any text using star mark (*)
-//get call it match between pattern bc
-// call /abbbcc ya abbc
-app.get("/ab*c", (req, res) => {
-  res.send("advance rounting call ab*c");
-});
-
-//4.reggex
-// => /a/ if in the path letter a is there is worked
-app.get(/a/, (req, res) => {
-  res.send("advance rounting call regex path in a");
-});
-// => /.*fly$/ start with any letter ends with fly letter ex./butterfly
-app.get(/.*fly$/, (req, res) => {
-  res.send("advance rounting call regex end with fly letter");
-});
-
-//5.make route dynamic using colon (:)
-// url => http:localhost:3000/user/101
-app.get("/user/:userId", (req, res) => {
-  console.log(req.params);
-});
-
-// 6.how to get userId from urlbar
-// url => htpp:localhost:3000/user?userId=100
-// query will give you the information about userId parameter
-app.get("/user", (req, res) => {
-  console.log(req.query);
-});
-
-// how  do you handle the incoming call using the route handler
-app.use("/", (req, res) => {
-  res.send("hello from the server");
+app.delete("/admin/deleteUser", (req, res) => {
+  res.send("user deleted sucessfully");
 });
