7e35a570acec169fced2cde4809619ada7eedff6
Ashokkumar Vaishnav <ashokranka30@gmail.com>
2024-11-27 23:52:44 +0530

    database connection

--- src/app.js
+++ src/app.js
@@ -1,46 +1,8 @@
-// create the server
 const express = require("express");
+require("./config/databse");
 
-// creating new web server using express
 const app = express();
 
-//  listen the incoming call this server create port number
-app.listen(3000);
-
-// server successfully listening call on port number 3000
-app.listen(500, () => {
-  console.log("server listen incoming call on port number 3000");
-});
-
-// middleware authrizaton example
-app.use("/admin", (req, res, next) => {
-  const token = "xyz";
-  const isAuthorized = token === "xyz";
-  if (!isAuthorized) {
-    res.status(401).send("unauthorized request");
-  } else {
-    next();
-  }
-});
-app.get("/admin/getData", (req, res) => {
-  res.send("user data is send");
-});
-app.delete("/admin/deleteUser", (req, res) => {
-  res.send("user deleted sucessfully");
-});
-
-// error handling
-
-app.get("/getUser", (req, res) => {
-  try {
-    res.send("user data send sucessfully");
-  } catch (err) {
-    res.send("error" + err);
-  }
-});
-
-app.use("/", (err, req, res, next) => {
-  if (err) {
-    res.status(401).send("something went wrong");
-  }
+app.listen(3000, () => {
+  console.log("server listening on port no 3000");
 });
---
+++ src/config/databse.js
@@ -0,0 +1,19 @@
+// username : ashokranka30
+// password : E_Bjh3jTSFSh5bk
+// connection string : "mongodb+srv://ashokranka30:E_Bjh3jTSFSh5bk@cluster0.zv0qp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  await mongoose.connect(
+    "mongodb+srv://ashokranka30:E_Bjh3jTSFSh5bk@cluster0.zv0qp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+  );
+};
+
+connectDB()
+  .then(() => {
+    console.log("connect to database sucessfully ");
+  })
+  .catch((err) => {
+    console.log("not connect to database", err);
+  });
